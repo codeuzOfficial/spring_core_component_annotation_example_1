53bf33ac26502e42df8bd0fd6e046af58f1216f6
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2023-06-17 16:40:53 +0500

    Init Commit

---
+++ src/main/java/dasturlash/uz/StudentService.java
@@ -0,0 +1,10 @@
+package dasturlash.uz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+    public void todo() {
+        System.out.println("Student Service TODO.");
+    }
+}
